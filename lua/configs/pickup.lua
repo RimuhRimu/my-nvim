062693cedc04b8f54024d96e9a2f6a6f9aece3f2
RimuRimuh <68438095+RimuhRimu@users.noreply.github.com>
2022-07-03 23:14:16 -0400

    configs for pickup plugin

---
+++ lua/configs/pickup.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+M.config = function ()
+  local pickup_is_available, pickup = pcall(require, "nvim-window-picker")
+  if pickup_is_available then
+    pickup.setup({
+      autoselect_one = true,
+      include_current_win = true,
+      selection_chars = "FJDKSLA;CMRUEIWOQP",
+      filter_func = nil,
+      filter_rules = {
+        bo = {
+          filetype = { "NvimTree", "neo-tree", "notify", "packer" },
+          buftype = { "terminal" }
+        }
+      },
+      fg_color = "#25db4a",
+      current_win_hl_color = "#1a1b26",
+      other_win_hl_color = "#24283d",
+    })
+  end
+end
+
+return M
